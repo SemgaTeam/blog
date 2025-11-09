039ff3f8ae2b89e40f21068202fb963d615ea1d7
dmxmss <dexammt@gmail.com>
2025-11-09 21:34:59 +0300

    handling all types of errors: app & echo

--- backend/internal/http/errorHandler.go
+++ backend/internal/http/errorHandler.go
@@ -5,16 +5,29 @@ import (
 	"github.com/labstack/echo/v4"
 
 	"errors"
+	"fmt"
 )
 
 func ErrorHandler(err error, c echo.Context) {
 	var appErr *e.AppError
+	var httpErr *echo.HTTPError
 
 	switch {
+	case errors.As(err, &appErr):
+		break
+	
+	case errors.As(err, &httpErr):
+		appErr = &e.AppError{
+			Code: httpErr.Code,
+			Message: fmt.Sprint(httpErr.Message),
+			Err: err,
+		}
+
 	case errors.Is(err, e.ErrPostNotFound):
 		appErr = e.BadRequest(err, "post not found")
+
 	default:
-		appErr = e.Internal(err)	
+		appErr = e.Internal(err)
 	}
 
 	if !c.Response().Committed {
