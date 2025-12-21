dd8ff12a269aff1dbe514b214d2a302f27edf0b3
dmxmss <dexammt@gmail.com>
2025-12-21 11:11:23 +0300

    GetMe tests

--- backend/internal/service/authService_test.go
+++ backend/internal/service/authService_test.go
@@ -324,3 +324,82 @@ func TestRefreshTokens(t *testing.T) {
 		}) 
 	}
 }
+
+func TestGetMe(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockTokenRepo := mock.NewMockTokenRepository(ctrl)
+	mockUserRepo := mock.NewMockUserRepository(ctrl)
+	mockHashRepo := mock.NewMockHashRepository(ctrl)
+
+	conf := config.GetConfig()
+
+	authService, err := NewAuthService(conf.Auth, mockTokenRepo, mockUserRepo, mockHashRepo)
+	if err != nil {
+		t.Errorf("error initialization auth service: conf = %v", conf)
+	}
+
+	tests := []struct{
+		testName string
+		userId int
+		wantError bool
+		setupMock func()
+	}{
+		{ 
+			testName: "success case", 
+			userId: 1,
+			wantError: false,
+			setupMock: func() {
+				mockUserRepo.
+					EXPECT().
+					GetUserById(gomock.Eq(1)).
+					Return(&entities.User{}, nil)	
+			},
+		},
+		{ 
+			testName: "invalid id", 
+			userId: -1,
+			wantError: true,
+			setupMock: func() {
+				mockUserRepo. 
+					EXPECT().
+					GetUserById(gomock.Eq(-1)).
+					Return(nil, errors.New("invalid id"))	
+			},
+		},
+		{
+			testName: "user not found",
+			userId: 1,
+			wantError: true,
+			setupMock: func() {
+				mockUserRepo. 
+					EXPECT().
+					GetUserById(gomock.Eq(1)).
+					Return(nil, errors.New("user not found"))	
+			},
+		},
+		{ 
+			testName: "user repository error", 
+			userId: 1,
+			wantError: true,
+			setupMock: func() {
+				mockUserRepo. 
+					EXPECT().
+					GetUserById(gomock.Eq(1)).
+					Return(nil, errors.New("repository error"))	
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			tt.setupMock()
+			_, err := authService.GetMe(context.Background(), tt.userId)
+
+			if (err != nil) != tt.wantError {
+				t.Errorf("GetMe() error = %v, wantError %v", err, tt.wantError)
+			}
+		}) 
+	}
+}
