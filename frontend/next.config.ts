e31f79fc92952d6c0dd4a20eb9dd41b0fcfc6eee
losion <losion445@gmail.com>
2026-01-17 08:12:55 +0300

    [scaffold fix] confings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
 
-export default nextConfig;
+  // если Go API на другом домене
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
+  }
+}
+
+export default nextConfig
