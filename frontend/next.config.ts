02adf8770726acff0c6c1bbd6b0fa2c8f11408b5
losion <losion445@gmail.com>
2026-02-06 12:22:12 +0300

    [fix] configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -11,7 +11,16 @@ const nextConfig: NextConfig = {
         hostname: "**"
       }
     ]
-  }
+  },
+    async rewrites() {
+    return [
+      {
+        source: '/api/:path*',            
+        destination: 'http://localhost:8080/api/:path*', 
+      },
+    ];
+  },
+  
 }
 
 export default nextConfig
